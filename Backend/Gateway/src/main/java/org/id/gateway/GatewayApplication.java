4940155e9d76f92aeba64f775ba7a4a6176a59b6
Hajer Noomene <hajer.noomene@gmail.com>
2022-11-21 15:26:02 +0100

    initial gateway implementation

--- Backend/Gateway/src/main/java/org/id/gateway/GatewayApplication.java
+++ Backend/Gateway/src/main/java/org/id/gateway/GatewayApplication.java
@@ -9,5 +9,8 @@ public class GatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
-
+@Bean
+	DiscoveryClientRouteDefinitionLocator dynamicRoutes(ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties dlp){
+		return new DiscoveryClientRouteDefinitionLocator(rdc,dlp);
+	}
 }
