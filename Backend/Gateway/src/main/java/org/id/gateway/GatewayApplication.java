ddb29fc85606665ff87079b1b960cc8a019fae3d
Hajer Noomene <hajer.noomene@gmail.com>
2022-11-21 20:45:16 +0100

    gateway import changes

--- Backend/Gateway/src/main/java/org/id/gateway/GatewayApplication.java
+++ Backend/Gateway/src/main/java/org/id/gateway/GatewayApplication.java
@@ -2,6 +2,10 @@ package org.id.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayApplication {
