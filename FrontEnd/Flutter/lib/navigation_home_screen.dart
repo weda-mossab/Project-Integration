c35dd17cad9a2698298f7c760258629f398071ed
Hajer Noomene <hajer.noomene@gmail.com>
2022-11-24 19:29:27 +0100

    list events

---
+++ FrontEnd/Flutter/lib/navigation_home_screen.dart
@@ -0,0 +1,78 @@
+import 'app_theme.dart';
+import 'custom_drawer/drawer_user_controller.dart';
+import 'custom_drawer/home_drawer.dart';
+import 'feedback_screen.dart';
+import 'help_screen.dart';
+import 'home_screen.dart';
+import 'invite_friend_screen.dart';
+import 'package:flutter/material.dart';
+
+class NavigationHomeScreen extends StatefulWidget {
+  @override
+  _NavigationHomeScreenState createState() => _NavigationHomeScreenState();
+}
+
+class _NavigationHomeScreenState extends State<NavigationHomeScreen> {
+  Widget? screenView;
+  DrawerIndex? drawerIndex;
+
+  @override
+  void initState() {
+    drawerIndex = DrawerIndex.HOME;
+    screenView = const MyHomePage();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: AppTheme.white,
+      child: SafeArea(
+        top: false,
+        bottom: false,
+        child: Scaffold(
+          backgroundColor: AppTheme.nearlyWhite,
+          body: DrawerUserController(
+            screenIndex: drawerIndex,
+            drawerWidth: MediaQuery.of(context).size.width * 0.75,
+            /* onDrawerCall: (DrawerIndex drawerIndexdata) {
+              changeIndex(drawerIndexdata);
+              //callback from drawer for replace screen as user need with passing DrawerIndex(Enum index)
+            },*/
+            screenView: screenView,
+          ),
+        ),
+      ),
+    );
+  }
+
+  void changeIndex(DrawerIndex drawerIndexdata) {
+    if (drawerIndex != drawerIndexdata) {
+      drawerIndex = drawerIndexdata;
+      switch (drawerIndex) {
+        case DrawerIndex.HOME:
+          setState(() {
+            screenView = const MyHomePage();
+          });
+          break;
+        case DrawerIndex.Help:
+          setState(() {
+            screenView = HelpScreen();
+          });
+          break;
+        case DrawerIndex.FeedBack:
+          setState(() {
+            screenView = FeedbackScreen();
+          });
+          break;
+        case DrawerIndex.Invite:
+          setState(() {
+            screenView = InviteFriend();
+          });
+          break;
+        default:
+          break;
+      }
+    }
+  }
+}
