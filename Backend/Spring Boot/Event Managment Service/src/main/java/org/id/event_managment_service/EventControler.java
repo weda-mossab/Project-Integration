515e141920b8128ceb372ccef2fffd19d089ff15
Mossab weda <mossabweda@gmail.com>
2022-12-01 12:11:11 +0100

    EventControler

--- Backend/Spring Boot/Event Managment Service/src/main/java/org/id/event_managment_service/EventControler.java
+++ Backend/Spring Boot/Event Managment Service/src/main/java/org/id/event_managment_service/EventControler.java
@@ -2,7 +2,6 @@ package org.id.event_managment_service;
 
 import java.util.List;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -10,7 +9,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @CrossOrigin("*")
 @RequestMapping("/api")
