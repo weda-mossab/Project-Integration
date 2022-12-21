d5bce77f2089ba7e5d445eb45120c9c73fbce852
Mossab weda <mossabweda@gmail.com>
2022-12-21 20:43:17 +0100

    create student service

---
+++ Backend/Spring Boot/Student Managment Service/id/src/main/java/org/id/id/IdApplication.java
@@ -0,0 +1,13 @@
+package org.id.id;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdApplication.class, args);
+	}
+
+}
