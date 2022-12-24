de4f713f85c3d1637149b56a93c3db5ef19091f1
Mossab weda <mossabweda@gmail.com>
2022-12-24 14:28:48 +0100

    commit

--- Backend/Spring Boot/Student Managment Service/student_managment_service/src/main/java/org/id/student_managment_service/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package org.id.student_managment_service;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource
-public interface StudentRepository extends MongoRepository<Student,String> {
-    
-}
