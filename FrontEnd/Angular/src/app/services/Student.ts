62552b969f165a8f4df403de17c077a067de1d05
salmaLM <lamouchisalma551@gmail.com>
2022-12-24 19:46:40 +0100

    afficher-student

--- FrontEnd/Angular/src/app/services/Student.ts
+++ FrontEnd/Angular/src/app/services/Student.ts
@@ -1,7 +1,8 @@
-export interface Event {
+export interface Student {
   sid: string;
-student_name: string;
-  date: string;
-  avatar: string;
-  description: string;
+  student_name: string;
+  Birth_date: string;
+  student_phone: string;
+  student_password: string;
+  student_class:string
 }
