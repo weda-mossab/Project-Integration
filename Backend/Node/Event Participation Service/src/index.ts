0a67de3930a628eb28d82de446997b83c1e30a94
naztarhex <qhmed0826@gmail.com>
2022-12-25 00:59:12 +0100

    End of day

--- Backend/Node/Event Participation Service/src/index.ts
+++ Backend/Node/Event Participation Service/src/index.ts
@@ -1,15 +1,19 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+const cors = require('cors');
 const keycloak = require('../src/keycloak-config.js').initKeycloak();
 dotenv.config();
 
+
 const app: Express = express();
 const port = process.env.PORT;
+app.use(cors());
+app.use(require('morgan')('dev'))
 app.use(keycloak.middleware());
 
-app.get('/',keycloak.protect(['user','admin']) , (req: Request, res: Response) => {
-  keycloak.
-  res.send('Refrech');
+app.get('/',keycloak.protect() , (req: Request, res: Response) => {
+
+  res.send(req);
 });
 
 app.listen(port, () => {
--- Backend/Node/Event Participation Service/src/keycloak-config.js
+++ Backend/Node/Event Participation Service/src/keycloak-config.js
@@ -8,9 +8,8 @@ var keycloakConfig = {
     bearerOnly: true,
     serverUrl: 'http://localhost:8080/auth',
     realm: 'Events',
-    credentials: {
-        secret: 'rdtkIU9BdiQYZzxBJ3I2UZVPTCKmNJWM'
-    }
+    
+    realmPublicKey: 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA1qTNRyao0kq8edwC3D2VvNu5wIuWJUqjC3SbA2uAT63Pk7eVTuZ4ZtgBDyF9yx2oZX9z/w+AfbuXe+yIWAhBgvF8sq6juaoIkGIPW7hXKJOmnwBPlZqNpjtbeXHGA4gYQlZS631rOAak1MAsC9FwrVYg4R77PSrzg2qWcTEkKrQzPwbhIIYIuWLha4NfDLHZFLfWyD2LwaiRK/IB7wMK2BNXhmqD25Lni/+P7f+2vH8SEE1MV6Dw77fM51G1vXbq2s0fopO172M+bK82D/8HPTIjNkVDRIjV0io+gjKnY57yN/IjG80eLrSLKBvv7fLxCWuZOJ969fbM7LIHvqOfzQIDAQAB'
 };
 
 function initKeycloak() {
