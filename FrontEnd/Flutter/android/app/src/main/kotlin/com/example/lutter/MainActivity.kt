0fe590494506e8676c5544daac44d4974f32b695
Hajer Noomene <hajer.noomene@gmail.com>
2022-12-26 18:02:42 +0100

    flutter fix

--- FrontEnd/Flutter/android/app/src/main/kotlin/com/example/lutter/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.lutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- FrontEnd/isetEvents_flutter/android/app/src/main/kotlin/com/example/clean_architecture_posts_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.clean_architecture_posts_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
