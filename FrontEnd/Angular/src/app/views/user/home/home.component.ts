3b5f38f893bd570291707f62ea720a216d97ea44
naztarhex <qhmed0826@gmail.com>
2022-12-29 00:40:09 +0100

    commit

--- FrontEnd/Angular/src/app/views/user/home/home.component.ts
+++ FrontEnd/Angular/src/app/views/user/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {AuthService} from 'src/app/auth/service/auth.service';
 
 @Component({
@@ -6,9 +6,13 @@ import {AuthService} from 'src/app/auth/service/auth.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  logedIn: boolean | undefined;
   
   constructor(public authService: AuthService ) {}
+  async ngOnInit(): Promise<void> {
+    this.logedIn = await this.authService.isLoggedIn();
+  }
 
 
 
