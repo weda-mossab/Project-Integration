57007e1367c59258d66b5e52dfc5990795a9a006
Hajer Noomene <hajer.noomene@gmail.com>
2022-12-28 10:05:20 +0100

    auth controller

--- FrontEnd/Flutter/lib/presentation/controllers/control/auth_binding.dart
+++ FrontEnd/Flutter/lib/presentation/controllers/control/auth_binding.dart
@@ -5,8 +5,5 @@ import 'package:event_app/presentation/controllers/control/auth_controller.dart'
 
 class AuthBinding extends Bindings {
   @override
-  void dependencies() {
-    Get.lazyPut(() => SignUpUseCase(Get.find<AuthenticationRepositoryIml>()));
-    Get.put(AuthController(Get.find()), permanent: true);
-  }
+  void dependencies() {}
 }
