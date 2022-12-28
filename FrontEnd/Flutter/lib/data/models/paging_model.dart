55b18cc920237fae66e447d1e0becabfab323f00
Hajer Noomene <hajer.noomene@gmail.com>
2022-12-28 13:39:01 +0100

    commit

--- FrontEnd/Flutter/lib/data/models/paging_model.dart
+++ FrontEnd/Flutter/lib/data/models/paging_model.dart
@@ -4,16 +4,15 @@ import 'package:event_app/domain/entities/paging.dart';
 class PagingModel extends Paging {
   PagingModel({
     required this.totalResults,
-    required this.articles,
-  }) : super(articles: articles, totalResults: totalResults);
+    required this.events,
+  }) : super(articles: events, totalResults: totalResults);
 
   final int totalResults;
-  final List<EventModel> articles;
+  final List<EventModel> events;
 
   @override
   factory PagingModel.fromJson(Map<String, dynamic> json) => PagingModel(
         totalResults: json["totalResults"],
-        articles:
-            List.from(json["articles"].map((x) => EventModel.fromJson(x))),
+        events: List.from(json["articles"].map((x) => EventModel.fromJson(x))),
       );
 }
