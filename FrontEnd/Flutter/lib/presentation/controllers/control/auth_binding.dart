98632b4e50402f1b7f77154433c1a6ce75e69c3e
Hajer Noomene <hajer.noomene@gmail.com>
2022-12-28 10:02:24 +0100

    controller fix

--- FrontEnd/Flutter/lib/presentation/controllers/control/auth_binding.dart
+++ FrontEnd/Flutter/lib/presentation/controllers/control/auth_binding.dart
@@ -1,7 +1,7 @@
 import 'package:get/get.dart';
 import 'package:event_app/data/repositories/auth_repository.dart';
 import 'package:event_app/domain/usecases/signup_use_case.dart';
-import 'package:event_app/presentation/controllers/auth/auth_controller.dart';
+import 'package:event_app/presentation/controllers/control/auth_controller.dart';
 
 class AuthBinding extends Bindings {
   @override
--- FrontEnd/Flutter/lib/presentation/pages/home/home_page.dart
+++ FrontEnd/Flutter/lib/presentation/pages/home/home_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 import 'package:event_app/app/config/app_colors.dart';
 import 'package:event_app/app/types/tab_type.dart';
-import 'package:event_app/presentation/controllers/auth/auth_controller.dart';
+import 'package:event_app/presentation/controllers/control/auth_controller.dart';
 import 'package:event_app/presentation/controllers/headline/headline_binding.dart';
 import 'package:event_app/presentation/pages/headline/headline_page.dart';
 import 'package:event_app/presentation/pages/profil/profile_page.dart';
