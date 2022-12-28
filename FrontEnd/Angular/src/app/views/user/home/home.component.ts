678b52ad97e26e8d81bfbfe835127543b016a077
naztarhex <qhmed0826@gmail.com>
2022-12-29 00:28:17 +0100

    Login fix

--- FrontEnd/Angular/src/app/views/user/home/home.component.ts
+++ FrontEnd/Angular/src/app/views/user/home/home.component.ts
@@ -7,12 +7,13 @@ import {AuthService} from 'src/app/auth/service/auth.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-isLoggedIn() {
-throw new Error('Method not implemented.');
-}
 
   constructor(private authService: AuthService ) {
-    authService.login()
-    authService.isLoggedIn()
+    
+
+  }
+
+  public login(){
+    this.authService.login()
   }
 }
