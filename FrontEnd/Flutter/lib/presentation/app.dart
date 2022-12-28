f61e3aabaf85277eaed2455fdbf899a98bc2b664
Hajer Noomene <hajer.noomene@gmail.com>
2022-12-28 01:23:43 +0100

    commit

---
+++ FrontEnd/Flutter/lib/presentation/app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:event_app/presentation/pages/home/home_page.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GetCupertinoApp(
+      initialRoute: "/",
+      home: HomePage(),
+    );
+  }
+}
