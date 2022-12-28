e9cc93264867cdb0ee78e65b5e47670c5193e4fb
Hajer Noomene <hajer.noomene@gmail.com>
2022-12-28 10:10:01 +0100

    denpendency

--- FrontEnd/Flutter/lib/presentation/controllers/control/auth_binding.dart
+++ FrontEnd/Flutter/lib/presentation/controllers/control/auth_binding.dart
@@ -1,5 +1,4 @@
 import 'package:get/get.dart';
-import 'package:event_app/data/repositories/auth_repository.dart';
 import 'package:event_app/domain/usecases/signup_use_case.dart';
 import 'package:event_app/presentation/controllers/control/auth_controller.dart';
 
