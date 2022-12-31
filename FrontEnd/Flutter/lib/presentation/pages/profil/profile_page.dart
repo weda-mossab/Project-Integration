0cdb8776de7c1a1fc65f3963bc77dfba93a24faa
Hajer Noomene <hajer.noomene@gmail.com>
2022-12-31 11:45:32 +0100

    profile page edited

--- FrontEnd/Flutter/lib/presentation/pages/profil/profile_page.dart
+++ FrontEnd/Flutter/lib/presentation/pages/profil/profile_page.dart
@@ -4,171 +4,147 @@ import 'package:get/get.dart';
 import 'package:event_app/presentation/pages/detail/detail_page.dart';
 import 'package:event_app/presentation/pages/headline/views/event_cell.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
+  @override
+  _EditProfilePageState createState() => _EditProfilePageState();
+}
+
+class _EditProfilePageState extends State<ProfilePage> {
+  bool showPassword = false;
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Card(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(15.0),
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.start,
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.only(left: 16, top: 25, right: 16),
+        child: GestureDetector(
+          onTap: () {
+            FocusScope.of(context).unfocus();
+          },
+          child: ListView(
             children: [
-              Container(
-                height: MediaQuery.of(context).size.height * .5,
+              Text(
+                "Profile",
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Center(
                 child: Stack(
                   children: [
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 40.0),
-                      child: Container(
-                        height: MediaQuery.of(context).size.height,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.only(
-                            bottomLeft: Radius.elliptical(
-                                MediaQuery.of(context).size.width * 0.5, 100.0),
-                            bottomRight: Radius.elliptical(
-                                MediaQuery.of(context).size.width * 0.5, 100.0),
-                          ),
-                        ),
-                      ),
+                    Container(
+                      width: 130,
+                      height: 130,
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                              width: 4,
+                              color: Theme.of(context).scaffoldBackgroundColor),
+                          boxShadow: [
+                            BoxShadow(
+                                spreadRadius: 2,
+                                blurRadius: 10,
+                                color: Colors.black.withOpacity(0.1),
+                                offset: Offset(0, 10))
+                          ],
+                          shape: BoxShape.circle,
+                          image: DecorationImage(
+                              fit: BoxFit.cover,
+                              image: NetworkImage(
+                                "https://i.pinimg.com/564x/64/df/be/64dfbeb7776852ca12b8942bde47f227.jpg",
+                              ))),
                     ),
-                    Align(
-                      alignment: Alignment.topCenter,
-                      child: Stack(
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.only(left: 10, top: 100),
-                            child: Align(
-                              alignment: Alignment.topCenter,
-                              child: TextButton(
-                                onPressed: () {
-                                  //send you to login page
-                                },
-                                child: Text(
-                                  'Press To Logout',
-                                  style: TextStyle(
-                                    color: Color.fromARGB(255, 15, 109, 185),
-                                  ),
-                                ),
-                              ),
+                    Positioned(
+                        bottom: 0,
+                        right: 0,
+                        child: Container(
+                          height: 40,
+                          width: 40,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            border: Border.all(
+                              width: 4,
+                              color: Theme.of(context).scaffoldBackgroundColor,
                             ),
+                            color: Color.fromARGB(255, 15, 109, 185),
                           ),
-                        ],
-                      ),
-                    ),
-                    Padding(
-                      padding:
-                          const EdgeInsets.only(left: 10, right: 10, top: 170),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          CircleAvatar(
-                            radius: 30,
-                            backgroundColor: Color.fromARGB(255, 15, 109, 185),
-                            child: Icon(
-                              Icons.chat,
-                              size: 30,
-                              color: Color.fromARGB(255, 210, 229, 253),
-                            ),
+                          child: Icon(
+                            Icons.edit,
+                            color: Colors.white,
                           ),
-                          CircleAvatar(
-                            radius: 70,
-                            backgroundImage: NetworkImage(
-                                'https://i.pinimg.com/originals/df/cd/79/dfcd797320e5340e606365f4047d1e79.jpg'),
-                          ),
-                          CircleAvatar(
-                            radius: 30,
-                            backgroundColor: Color.fromARGB(255, 15, 109, 185),
-                            child: Icon(
-                              Icons.call,
-                              size: 30,
-                              color: Color.fromARGB(255, 210, 229, 253),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
+                        )),
                   ],
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.only(
-                    left: 121, right: 10, top: 1, bottom: 40),
-                child: Text('User Name', style: TextStyle(fontSize: 20)),
+              SizedBox(
+                height: 35,
               ),
-              Container(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 150.0),
-                      child: Column(
-                        children: [
-                          Text('Gender',
-                              style: TextStyle(
-                                color: Color.fromARGB(255, 15, 109, 185),
-                              )),
-                          SizedBox(
-                            height: 15,
-                          ),
-                          Text(
-                            'Female',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 150.0),
-                      child: Column(
-                        children: [
-                          Text('Phone number',
-                              style: TextStyle(
-                                color: Color.fromARGB(255, 15, 109, 185),
-                              )),
-                          SizedBox(
-                            height: 15,
-                          ),
-                          Text(
-                            '52-698-741',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 150.0),
-                      child: Column(
-                        children: [
-                          Text('Birth date',
-                              style: TextStyle(
-                                color: Color.fromARGB(255, 15, 109, 185),
-                              )),
-                          SizedBox(
-                            height: 15,
-                          ),
-                          Text(
-                            '12/06/2000',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
+              buildTextField("Full Name", "User Name", false),
+              buildTextField("E-mail", "userName123@gmail.com", false),
+              buildTextField("Password", "********", true),
+              buildTextField("Location", "Tunisia, Bizerte ", false),
+              buildTextField("Phone number", "78-123-456", false),
+              SizedBox(
+                height: 35,
               ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  TextButton(
+                    onPressed: () {},
+                    child: Text("CANCEL",
+                        style: TextStyle(
+                            fontSize: 14,
+                            letterSpacing: 2.2,
+                            color: Color.fromARGB(255, 255, 0, 0))),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Text(
+                      "SAVE",
+                      style: TextStyle(
+                          fontSize: 14,
+                          letterSpacing: 2.2,
+                          color: Colors.white),
+                    ),
+                  )
+                ],
+              )
             ],
           ),
         ),
+      ),
+    );
+  }
+
+  Widget buildTextField(
+      String labelText, String placeholder, bool isPasswordTextField) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 35.0),
+      child: TextField(
+        obscureText: isPasswordTextField ? showPassword : false,
+        decoration: InputDecoration(
+            suffixIcon: isPasswordTextField
+                ? IconButton(
+                    onPressed: () {
+                      setState(() {
+                        showPassword = !showPassword;
+                      });
+                    },
+                    icon: Icon(
+                      Icons.remove_red_eye,
+                      color: Colors.grey,
+                    ),
+                  )
+                : null,
+            contentPadding: EdgeInsets.only(bottom: 3),
+            labelText: labelText,
+            floatingLabelBehavior: FloatingLabelBehavior.always,
+            hintText: placeholder,
+            hintStyle: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            )),
       ),
     );
   }
