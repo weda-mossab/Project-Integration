0165385898902de6ef7ff0127d077ae3c00e2cea
Mossab weda <mossabweda@gmail.com>
2023-01-10 12:21:17 +0100

    Let

--- FrontEnd/Angular/src/app/layouts/admin-layout/admin-layout.component.ts
+++ FrontEnd/Angular/src/app/layouts/admin-layout/admin-layout.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import {AuthService} from 'src/app/auth/service/auth.service';
-
+import { KeycloakProfile } from 'keycloak-js';
+import { AuthService } from 'src/app/auth/service/auth.service';
 
 @Component({
   selector: 'app-admin-layout',
@@ -9,8 +9,16 @@ import {AuthService} from 'src/app/auth/service/auth.service';
 })
 export class AdminLayoutComponent {
 
+  logedIn: boolean | undefined;
+  profile: KeycloakProfile | undefined
+  Admin: boolean | undefined;
   constructor(public authService: AuthService ) {}
-  
- 
+
+  async ngOnInit(): Promise<void> {
+    this.logedIn = await this.authService.isLoggedIn();
+    this.profile= await this.authService.getProfile();
+    this.Admin = this.authService.getRoles().includes("admin")
+    console.log(this.Admin)
+  }
 
 }
--- FrontEnd/Angular/src/app/views/admin/dashboard/dashboard/dashboard.component.ts
+++ FrontEnd/Angular/src/app/views/admin/dashboard/dashboard/dashboard.component.ts
@@ -1,11 +1,11 @@
 import { Component ,OnInit} from '@angular/core';
-
 import {FormBuilder, FormGroup} from "@angular/forms";
-// import { Event, Event } from 'src/app/event';
 import { Observable } from 'rxjs';
 import { EventService } from 'src/app/services/event.service';
-
 import { Student } from 'src/app/services/Student';
+import { ActivatedRoute } from '@angular/router';
+
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -13,26 +13,31 @@ import { Student } from 'src/app/services/Student';
 })
 export class DashboardComponent implements OnInit {
   private students?:  Student[];
-  constructor(private eventService: EventService ) {}
+  constructor(private eventService: EventService,private route: ActivatedRoute ) {}
+
   ngOnInit(): void {
-   
-    this.eventService.getParticipta("Event2").subscribe(data => {
-   
+    this.sub = this.route.params.subscribe(params => {
+    this.eventService.getParticipta(params['id']).subscribe(data => {
       this.Students=data;
+      console.log(data)
 
     })
+  });
 
-}
 
+
+ }
+
+  // getParticipta():void {
+  //   this.eventService.getParticipta()
+  //   .subscribe(events => this.Students = students);
+  // }
   Students: Student[] = [];
   searchText: string = '';
-
-
-
 
 onSearchTextEntered(searchValue: string){
 this.searchText=searchValue;
 console.log(this.searchText)
 }
-
+sub: any;
 }
